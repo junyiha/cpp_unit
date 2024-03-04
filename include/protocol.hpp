5fee3c92a14df19a9ca45662c5994010d2df5596
junyiha <zhangjunyiha@gmail.com>
2024-03-04 17:14:12 +0800

    build project

---
+++ include/protocol.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file protocol.hpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-03-04
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#pragma once
+#ifndef CPP_UNIT_PROTOCOL_HPP
+#define CPP_UNIT_PROTOCOL_HPP
+
+#include <string>
+#include <iostream>
+
+namespace Protocol
+{
+
+}  // namespace Protocol
+
+#endif  // CPP_UNIT_PROTOCOL_HPP
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-03-04
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "protocol.hpp"
+
+int main(int argc, char* argv[])
+{
+    std::clog << "--cpp unit--" << std::endl;
+
+    return 0;
+}
